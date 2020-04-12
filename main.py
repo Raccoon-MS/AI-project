511de047079dd53ebfa67391d4b6d0152ddc5887
Raccoon-MS <sanniermathis@gmail.com>
2020-04-12 14:33:20 +0200

    multiple weights

--- main.py
+++ main.py
@@ -3,26 +3,36 @@ import random as rnd
 import math
 
 class Neuron():
-    def __init__(self):
-        self.weight = rnd.random()
-        self.output = 0
+    def __init__(self, layer, id, numberOfWeights):
+        self.id = id
+        self.layer = layer
+        if numberOfWeights == 0:
+            self.weights = [1]
+        else:
+            self.weights = []
+            for i in range(0, numberOfWeights):
+                self.weights.append(rnd.uniform(-0.01,0.01))
+
+        self.bias = rnd.random()
+        self.output = (0,0)
 
     def think(self, inputs):
         self.output = 0
         value = 0
         for i in inputs:
             v, w = i
-            value += w*v
-        self.output = (self.sigmoid(value), self.weight)
+            value += w[self.id]*v
+        self.output = (self.sigmoid(self.bias + value), self.weights)
             
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+        return 1 / (1 + math.exp(-x))
 
 class NeuronLayer():
-    def __init__(self, numberOfNeurons):
+    def __init__(self, id, numberOfNeurons, nextLayerSize):
+        self.id = id
         self.neurons = []
         for i in range(0, numberOfNeurons):
-            self.neurons.append(Neuron())
+            self.neurons.append(Neuron(id, i, nextLayerSize))
     
     def think(self, inputs):
         for n in self.neurons:
@@ -38,7 +48,10 @@ class NeuralNetwork():
     def __init__(self, params):
         self.layers = []
         for i in range(0, len(params)):
-            self.layers.append(NeuronLayer(params[i]))
+            nextLayerSize = 0
+            if i < len(params) - 1:
+                nextLayerSize = params[i+1]
+            self.layers.append(NeuronLayer(i, params[i], nextLayerSize))
 
     def think(self, inputs):
         self.layers[0].think(inputs)
@@ -53,10 +66,78 @@ class NeuralNetwork():
 
         pass
 
+class InputConverter():
+    def __init__(self, file):
+        self.file = file
+        self.data = []
+        self.normalAlphabet = []
+        self.codedAlphabet = {}
+
+    def convert(self):
+        lines = open(self.file, "r")
+        self.createAlphabet(lines)
+        lines.close()
+        self.codeAlphabet()
+        lines = open(self.file, "r")
+        self.translateAll(lines)
+        lines.close()
+        return self.data
+
+    def createAlphabet(self, lines):
+        for l in lines:
+            if l.endswith("\n"):
+                l = l[:-1]
+            words = l.split(" ")
+            words = words[:-1]
+            for w in words:
+                if not w in self.normalAlphabet:
+                    self.normalAlphabet.append(w)
+    
+    def codeAlphabet(self):
+        maxi = len(self.normalAlphabet) + 1
+        index = 0
+        for w in self.normalAlphabet:
+            code = index / maxi
+            self.codedAlphabet[w] = code
+            index += 1
+
+    def translateAll(self, lines):
+        for l in lines:
+            if l.endswith("\n"):
+                l = l[:-1]
+
+            words = l.split(" ")
+
+            result = int(words[-1])
+            words = words[:-1]
+            
+            sentence = []
+            for w in words:
+                sentence.append(self.translateWord(w))
+            self.data.append((sentence, result))
+            
+    def translateWord(self, word):
+        return self.codedAlphabet[word]
+
+    def maxSentenceLength(self):
+        maxi = 0
+        for d in self.datas:
+            s, r = d
+            if len(s) > maxi:
+                maxi = len(s)
+        return maxi 
+
+
 if __name__ == "__main__":
-    archi = [3, 5, 1]
+    datas = InputConverter("dataset.csv").convert()
+    inputLayerSize = datas.maxSentenceLength()
+    
+    archi = [inputLayerSize, 15, 1]
     network = NeuralNetwork(archi)
-    print(network.think([(1,1), (0,1), (0,0)]))
+    result = network.think([(1,[1,1]), (1,[1,1])])
+    for r in result:
+        v, w = r
+        print(v)"""
     
             
 
